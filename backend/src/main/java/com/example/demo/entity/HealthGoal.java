dd5e80bb6e0e531ef13e81bb57591b618c850b9e
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 19:05:50 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthGoal.java
+++ backend/src/main/java/com/example/demo/entity/HealthGoal.java
@@ -1,6 +1,10 @@
 package com.example.demo.entity;
 
-@Entity5
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="health_goal")
 public class HealthGoal {
     
 }
