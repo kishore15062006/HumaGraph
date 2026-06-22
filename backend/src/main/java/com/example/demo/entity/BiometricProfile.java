a69be7846d128d2923650f26d088567f4b446d83
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 12:16:03 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BiometricProfile.java
+++ backend/src/main/java/com/example/demo/entity/BiometricProfile.java
@@ -1,5 +1,12 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="biometric_profile")
 public class BiometricProfile {
+    private long id;
+    private UserAccount userAccount;
     
 }
