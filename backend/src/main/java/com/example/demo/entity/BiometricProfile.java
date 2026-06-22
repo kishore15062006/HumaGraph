da85fb12e5da0e8c7f1a2f74781c259cd35b4605
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 12:25:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/BiometricProfile.java
+++ backend/src/main/java/com/example/demo/entity/BiometricProfile.java
@@ -2,12 +2,14 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,7 +18,8 @@ public class BiometricProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @JoinColumn(name="user_account_id")
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="user_account_id",nullable = false,unique = true)
     private UserAccount userAccount;
     @Column(nullable = false)
     private String fullName;
