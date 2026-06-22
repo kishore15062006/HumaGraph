94b5e112b8c11ab3590f3afb6e4cdfe291193c18
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 19:07:20 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthGoal.java
+++ backend/src/main/java/com/example/demo/entity/HealthGoal.java
@@ -1,10 +1,18 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="health_goal")
 public class HealthGoal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @ManyToOne
     
 }
