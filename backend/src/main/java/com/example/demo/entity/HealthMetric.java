517810ec70d86df90b1a23a6cdca169d77f56ef1
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 12:31:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthMetric.java
+++ backend/src/main/java/com/example/demo/entity/HealthMetric.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,7 +13,9 @@ public class HealthMetric {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(unique = true)
     private String name;
     private String unit;
     private MetricCategory category;
+    private boolean isSystemStandard=true;
 }
--- backend/src/main/java/com/example/demo/entity/MetricCategory.java
+++ backend/src/main/java/com/example/demo/entity/MetricCategory.java
@@ -1,5 +1,8 @@
 package com.example.demo.entity;
 
 public enum MetricCategory {
-
+    CARDIO,
+    METABOLIC,
+    FITNESS,
+    SLEEP
 }
