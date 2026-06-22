7bbc77bb20662f5e186f81ac57ff1a734f2ae1b2
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 12:13:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/UserAccount.java
+++ backend/src/main/java/com/example/demo/entity/UserAccount.java
@@ -7,10 +7,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.Table;
 
-@Data
 @Entity
+@Table(name="user_account")
 public class UserAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
