ef872684910c65e8fbc73718f4603cebaafa7126
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 12:20:34 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/UserAccount.java
+++ backend/src/main/java/com/example/demo/entity/UserAccount.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,7 +21,7 @@ public class UserAccount {
     private String email;
     @Column(nullable = false)
     private String passwordHash;
-    @Enumerated
+    @Enumerated(EnumType.STRING)
     private UserRole role;
     @Column(nullable = false)
     private boolean isActive=true;
