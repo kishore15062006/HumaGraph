d6f90d00241f5e3dfcf4b542068d3e0b7efee506
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-22 12:28:04 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthMetric.java
+++ backend/src/main/java/com/example/demo/entity/HealthMetric.java
@@ -1,5 +1,16 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="health_metric")
 public class HealthMetric {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 }
