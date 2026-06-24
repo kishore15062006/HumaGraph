296fbe8269471dd5b30b1e9a94f16ae3f2e67749
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 12:26:37 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthMetricController.java
+++ backend/src/main/java/com/example/demo/controller/HealthMetricController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.HealthMetricService;
@@ -10,5 +11,8 @@ public class HealthMetricController {
     
     @Autowired
     private HealthMetricService healthMetricService;
+
+    @GetMapping("/get")
+    public 
 
 }
