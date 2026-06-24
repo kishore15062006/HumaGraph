028d425b0002a7497034b31b588bcf7c14f28245
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 11:22:55 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/HealthReadingRepositoy.java
+++ backend/src/main/java/com/example/demo/repository/HealthReadingRepositoy.java
@@ -1,5 +1,11 @@
 package com.example.demo.repository;
 
-public interface HealthReadingRepositoy {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.HealthReading;
+
+@Repository
+public interface HealthReadingRepositoy extends JpaRepository<HealthReading,Long>{
     
 }
