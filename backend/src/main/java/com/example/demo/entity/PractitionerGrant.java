f8ecdbbde27fc510b1cf073bd8be6614138546b1
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 10:57:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/PractitionerGrant.java
+++ backend/src/main/java/com/example/demo/entity/PractitionerGrant.java
@@ -1,10 +1,16 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="practio")
+@Table(name="practitioner_grant")
 public class PractitionerGrant {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private UserAccount practitionerAccount;
 }
