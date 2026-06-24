3f3c2d947f4cc20f49bd5a3e10a508a9ce134957
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 12:16:07 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/PractitionerGrantRepository.java
+++ backend/src/main/java/com/example/demo/repository/PractitionerGrantRepository.java
@@ -1,6 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ import com.example.demo.entity.PractitionerGrant;
 @Repository
 public interface PractitionerGrantRepository extends JpaRepository<PractitionerGrant,Long>{
     List<PractitionerGrant> findByPractitionerAccountId(Long practitionerId);
-    List<PractitionerGrant> findByPatientProfileId
+    List<PractitionerGrant> findByPatientProfileId(Long profileId);
+    Optional<PractitionerGrant> findByPractitionerAccountIdAndPatientProfileId(Long practitionerId,Long profileId);
 }
