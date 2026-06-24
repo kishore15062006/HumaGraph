52f96bcde9b3259396214d3d60d6a712a511e2bb
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 12:17:37 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/HealthMetricService.java
+++ backend/src/main/java/com/example/demo/service/HealthMetricService.java
@@ -1,5 +1,12 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class HealthMetricService {
+
+    @Autowired
+    private Heal
     
 }
