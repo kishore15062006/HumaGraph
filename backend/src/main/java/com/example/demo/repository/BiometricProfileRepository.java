4a73570259eedc23afd3e0a5b3d20f75181c4936
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 11:18:26 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/BiometricProfileRepository.java
+++ backend/src/main/java/com/example/demo/repository/BiometricProfileRepository.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.BiometricProfile;
-import com.example.demo.entity.UserAccount;
+
 
 @Repository
 public interface BiometricProfileRepository extends JpaRepository<BiometricProfile,Long>{
-    Optional<UserAccount> findByEmail(String email);
+    Optional<BiometricProfile> findByUserAccountId(Long userId);
     
 }
--- backend/src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ backend/src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.demo.entity.UserAccount;
 
 @Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount,Long> {
-    
+    Optional<UserAccount> findByEmaiOptional9s
 }
