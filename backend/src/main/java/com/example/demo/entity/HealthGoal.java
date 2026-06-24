8492501c719ee0cb80255c84b18b5dc601e3a0c1
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 10:51:25 +0530

    User commit

--- backend/src/main/java/com/example/demo/entity/HealthGoal.java
+++ backend/src/main/java/com/example/demo/entity/HealthGoal.java
@@ -2,7 +2,9 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,6 +23,8 @@ public class HealthGoal {
     private HealthMetric metric;
     private double targetValue;
     private double currentValue=0;
+    @Column(nullable = false)
     private LocalDate targetDate;
+    @Enumerated(e)
     private GoalStatus status;
 }
