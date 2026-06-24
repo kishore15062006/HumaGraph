3c14c355731c699b8fe935968e6cce510d295dd0
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-24 12:10:07 +0530

    User commit

--- backend/src/main/java/com/example/demo/repository/HealthGoalRepository.java
+++ backend/src/main/java/com/example/demo/repository/HealthGoalRepository.java
@@ -1,5 +1,13 @@
 package com.example.demo.repository;
 
-public interface HealthGoalRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.HealthGoal;
+
+@Repository
+public interface HealthGoalRepository extends JpaRepository<HealthGoal,Long> {
+    List<HealthGoal> findByProfileIdAndStatus
 }
