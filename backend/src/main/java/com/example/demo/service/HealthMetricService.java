076586ee00e6fe6b549a483c0400f5fcc6a926ce
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-25 08:33:43 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/HealthMetricService.java
+++ backend/src/main/java/com/example/demo/service/HealthMetricService.java
@@ -3,7 +3,6 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.HealthMetric;
@@ -15,7 +14,7 @@ public class HealthMetricService {
     @Autowired
     private HealthMetricRepository healthMetricRepo;
 
-    public ResponseEntity<?> getAllMetrics(){
+    public List<HealthMetric> getAllMetrics(){
         return healthMetricRepo.findAll();
     }
 
