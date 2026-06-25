cd32fb802132d2cb0ab9fc1ea1b1886a79a95d3a
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-25 08:36:43 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthReadingController.java
+++ backend/src/main/java/com/example/demo/controller/HealthReadingController.java
@@ -2,16 +2,19 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.HealthReadingService;
 
 @RestController
+@RequestMapping("/api")
 public class HealthReadingController {
     
     @Autowired
     private HealthReadingService healthReadingService;
 
-    @GetMapping("/")
+    @GetMapping("/readings")
+    public List
 
 }
