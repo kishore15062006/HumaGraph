f79f80a7b8d37500433e647f9a3419c78e8e4ec8
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-25 09:15:10 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthMetricController.java
+++ backend/src/main/java/com/example/demo/controller/HealthMetricController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +22,8 @@ public class HealthMetricController {
 
     @GetMapping
     public ResponseEntity<List<HealthMetric>> getAllMetrics(){
-        return healthMetricService.getAllMetrics();
+        List<HealthMetric> metrics=healthMetricService.getAllMetrics();
+        return ResponseEntity.ok(metrics);
     }
 
 }
