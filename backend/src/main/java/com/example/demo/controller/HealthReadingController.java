1ef8fce09a2013f6d8739c4eedb5ac93c73c7ea7
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-25 08:39:43 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthReadingController.java
+++ backend/src/main/java/com/example/demo/controller/HealthReadingController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,9 @@ public class HealthReadingController {
 
     @GetMapping("/readings")
     public List<ReadingResponseDto> getReadings(){
-        
+        return healthReadingService.getReadings();
     }
+
+    @PostMapping()
 
 }
--- backend/src/main/java/com/example/demo/service/HealthReadingService.java
+++ backend/src/main/java/com/example/demo/service/HealthReadingService.java
@@ -1,8 +1,11 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.dto.ReadingResponseDto;
 import com.example.demo.repository.HealthReadingRepository;
 
 @Service
@@ -11,6 +14,11 @@ public class HealthReadingService {
     @Autowired
     private HealthReadingRepository healthReadingRepo;
 
-    
+    public List<ReadingResponseDto> getReadings() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getReadings'");
+    }
+
+
 
 }
