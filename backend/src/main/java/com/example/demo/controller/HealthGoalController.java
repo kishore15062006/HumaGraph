15086c76b58a7510e580d6cc195a862195dbe493
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-25 09:21:11 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthGoalController.java
+++ backend/src/main/java/com/example/demo/controller/HealthGoalController.java
@@ -1,5 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.HealthGoalService;
+
+@RestController
+@RequestMapping("/api/goals")
 public class HealthGoalController {
+
+    @Autowired
+    private final HealthGoalService goalService
+
     
 }
