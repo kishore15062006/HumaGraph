7007c4be6f1404fe985fa28d1e878d427e142a45
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-25 08:56:13 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/HealthReadingController.java
+++ backend/src/main/java/com/example/demo/controller/HealthReadingController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,5 +29,8 @@ public class HealthReadingController {
     public ReadingResponseDto addReadings(@RequestBody ReadingResponseDto readingResponse){
         return healthReadingService.addReadings(readingResponse);
     }
+
+    @DeleteMapping("/readings{id}")
+    public void 
 
 }
