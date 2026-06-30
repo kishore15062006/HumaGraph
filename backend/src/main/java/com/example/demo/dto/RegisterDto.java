c475c1a042568007ee9ca7a3605a6d9bf8dcf6ef
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-30 12:02:01 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/RegisterDto.java
+++ backend/src/main/java/com/example/demo/dto/RegisterDto.java
@@ -1,8 +1,7 @@
 package com.example.demo.dto;
 
-import lombok.Data;
+import java.time.LocalDate;
 
-@Data
 public class RegisterDto {
 
     private String email;
@@ -10,5 +9,39 @@ public class RegisterDto {
     private String password;
 
     private String fullName;
+
+    private LocalDate dateOfBirth;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email=email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password=password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName=fullName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth=dateOfBirth;
+    }
 
 }
