5e0141b9adc7917f36e3c68ea27d0028b681499c
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-30 12:44:01 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/HealthGoalService.java
+++ backend/src/main/java/com/example/demo/service/HealthGoalService.java
@@ -3,10 +3,12 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.GoalResponseDto;
 import com.example.demo.repository.HealthGoalRepository;
 
+@Service
 public class HealthGoalService {
 
     @Autowired
