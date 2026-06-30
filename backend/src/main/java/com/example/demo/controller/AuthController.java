6e3141ffa0fc7b4ea031e5b985d40fa1b3feb702
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-30 11:50:01 +0530

    User commit

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,48 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.dto.AuthRequestDto;
+import com.example.demo.dto.AuthResponseDto;
+import com.example.demo.dto.RegisterDto;
+
+import com.example.demo.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(
+
+            @RequestBody RegisterDto dto) {
+
+        return ResponseEntity.ok(
+
+                authService.register(dto)
+
+        );
+
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(
+
+            @RequestBody AuthRequestDto dto) {
+
+        return ResponseEntity.ok(
+
+                authService.login(dto)
+
+        );
+
+    }
+
 }
