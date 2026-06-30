1fd0cec2fb2d20ccf6e4ee9366d3f136a8b1b224
KISHORE P <727724eucb025@skcet.ac.in>
2026-06-30 12:26:01 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/HealthGoalService.java
+++ backend/src/main/java/com/example/demo/service/HealthGoalService.java
@@ -5,14 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.dto.GoalResponseDto;
+import com.example.demo.repository.HealthGoalRepository;
 
 public class HealthGoalService {
 
     @Autowired
-    private 
+    private HealthGoalRepository goalRepository;
 
     public List<GoalResponseDto> getGoalsByUser(long id) {
-        
+        return goalRepository.getGoalsByUser(id);
     }
     
 }
