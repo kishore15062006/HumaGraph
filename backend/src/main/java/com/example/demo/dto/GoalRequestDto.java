74a0bca9243e437f6b3d3602e4e1bf714d4bd914
KISHORE P <727724eucb025@skcet.ac.in>
2026-07-02 12:06:13 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/GoalRequestDto.java
+++ backend/src/main/java/com/example/demo/dto/GoalRequestDto.java
@@ -1,5 +1,18 @@
 package com.example.demo.dto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class GoalRequestDto {
-    
+    Long metricId;
+    Double targetValue;
+    LocalDate targetDate;
 }
--- backend/src/main/java/com/example/demo/dto/GoalResponseDto.java
+++ backend/src/main/java/com/example/demo/dto/GoalResponseDto.java
@@ -1,5 +1,21 @@
 package com.example.demo.dto;
 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class GoalResponseDto {
-    
+    Long id
+String metricName
+Double targetValue
+    Double currentValue;
+LocalDate targetDate;
+String status;
 }
