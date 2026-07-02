b70f5bc96ad72c06267f85ae7f3a35f0f776f0cc
KISHORE P <727724eucb025@skcet.ac.in>
2026-07-02 11:28:43 +0530

    User commit

--- backend/src/main/java/com/example/demo/dto/DailySummaryDto.java
+++ backend/src/main/java/com/example/demo/dto/DailySummaryDto.java
@@ -1,5 +1,14 @@
 package com.example.demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+
 public class DailySummaryDto {
-    
+    private String day;
+    private double average;
 }
