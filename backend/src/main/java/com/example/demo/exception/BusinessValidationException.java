b3d9c0bc9b3061d41a3c9ca05b7e9a52b3e87c69
KISHORE P <727724eucb025@skcet.ac.in>
2026-07-02 11:24:13 +0530

    User commit

--- backend/src/main/java/com/example/demo/exception/BusinessValidationException.java
+++ backend/src/main/java/com/example/demo/exception/BusinessValidationException.java
@@ -1,5 +1,8 @@
 package com.example.demo.exception;
 
-public class BusinessValidationException extends {
-    
+public class BusinessValidationException extends RuntimeException {
+
+    public BusinessValidationException(String message) {
+        super(message);
+    }
 }
