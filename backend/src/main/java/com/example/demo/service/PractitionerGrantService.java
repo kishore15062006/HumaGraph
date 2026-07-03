f4fba7106ffbb66d8be9168b387a84b55b22f5fc
KISHORE P <727724eucb025@skcet.ac.in>
2026-07-03 09:04:06 +0530

    User commit

--- backend/src/main/java/com/example/demo/service/PractitionerGrantService.java
+++ backend/src/main/java/com/example/demo/service/PractitionerGrantService.java
@@ -4,7 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
