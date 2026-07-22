550af4cb2e77bb9a2f4b7570f3a76c989cfdbd63
KISHORE P <727724eucb025@skcet.ac.in>
2026-07-22 09:18:54 +0530

    User commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,8 +5,10 @@ const Login = () => {
     <div>
         <form>
             <h1>Login</h1>
-            <label>Email Address</label>
-            <input type='email' placeholder='Email Address'>
+            <label htmlFor="Email Address"></label>
+            <input type='email' placeholder='patient@example.com' required>
+            <label htmlFor="Password"></label>
+            <input type='password' place></input>
             </input>
         </form>
     </div>
