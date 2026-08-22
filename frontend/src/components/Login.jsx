9957022816d012311c25c4ece045092d92b66d21
KISHORE P <727724eucb025@skcet.ac.in>
2026-08-22 09:08:55 +0530

    User commit

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { login } from "../../store/authSlice";
+import { login } from "../store/slices/authSlice";
 
 const Login = () => {
   const dispatch = useDispatch();
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { registerUser } from "../../store/authSlice";
+import { registerUser } from "../store/slices/authSlice";
 import "./Register.css";
 
 const Register = () => {
