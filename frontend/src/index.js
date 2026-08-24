7d1d4a03eba271a9a09822915a464935c8b02027
KISHORE P <727724eucb025@skcet.ac.in>
2026-08-24 15:44:13 +0530

    User commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import index from "./index"
+import index from "./store/index"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-        <Provider store={store}>
+        <Provider store={index}>
             <App />
         </Provider>
     </React.StrictMode>
