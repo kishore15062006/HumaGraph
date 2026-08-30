a462a5e646abe26493eaef8faadac1f2c8102191
KISHORE P <727724eucb025@skcet.ac.in>
2026-08-30 10:40:00 +0530

    User commit

--- frontend/src/components/dashboard/AdminDashboard.jsx
+++ frontend/src/components/dashboard/AdminDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import api from "../../services/api";
 import StatCards from "./StatCard";
+import "./AdminDashboard.css"
 
 const AdminDashboard = () => {
   const [users, setUsers] = useState([]);
