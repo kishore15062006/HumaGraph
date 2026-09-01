ee56a6451376912afead4958433aefd65148d925
kishore15062006 <kishore15062006@gmail.com>
2026-09-01 12:52:19 +0530

    1st sept

--- backend/src/main/java/com/example/demo/entity/BiometricProfile.java
+++ backend/src/main/java/com/example/demo/entity/BiometricProfile.java
@@ -2,7 +2,6 @@ package com.example.demo.entity;
 
 import java.time.LocalDate;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
